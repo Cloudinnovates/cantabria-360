cd3a8663f72068fbd864d179b381f60bd037e9d6
Rubén Chavarría <txingo@gmail.com>
2019-03-18 21:14:36 +0100

    Run standard lint for the first time

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
@@ -9,4 +9,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   }
-};
+}
