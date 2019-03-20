ed54a7e760a73b04ecc5bd63256036caaf32e59e
Rubén Chavarría <txingo@gmail.com>
2019-03-20 22:39:05 +0100

    Add gate from bedroom to entrance

--- src/data/tours.js
+++ src/data/tours.js
@@ -3,21 +3,21 @@ import { Vector3 } from 'three'
 import RoomModel from '../model/room-model'
 import TourModel from '../model/tour-model'
 
-const bedroomGate = new GateModel({
+const fromEntranceToBedroom = new GateModel({
   id: 'from-entrance-to-bedroom',
   label: 'Habitación',
   position: new Vector3(175, 0, -300),
   goesTo: 'bedroom'
 })
 
-const kitchenGate = new GateModel({
+const fromEntranceToKitchen = new GateModel({
   id: 'from-entrance-to-kitchen',
   label: 'Cocina',
   position: new Vector3(300, 0, 0),
   goesTo: 'kitchen'
 })
 
-const bathroomGate = new GateModel({
+const fromEntranceToBethroom = new GateModel({
   id: 'from-entrance-to-bathroom',
   label: 'Baño',
   position: new Vector3(300, 0, -200),
@@ -28,16 +28,25 @@ const entranceRoom = new RoomModel({
   id: 'entrance',
   panorama: 'assets/images/entrance.jpg',
   gates: [
-    bedroomGate,
-    kitchenGate,
-    bathroomGate
+    fromEntranceToBedroom,
+    fromEntranceToKitchen,
+    fromEntranceToBethroom
   ]
+})
+
+const fromBedroomToEntrance = new GateModel({
+  id: 'from-bedroom-to-entrance',
+  label: 'Entrada',
+  position: new Vector3(175, 0, -300),
+  goesTo: 'entrance'
 })
 
 const bedroomRoom = new RoomModel({
   id: 'bedroom',
   panorama: 'assets/images/bedroom.jpg',
-  gates: []
+  gates: [
+    fromBedroomToEntrance
+  ]
 })
 
 const kitchenRoom = new RoomModel({
