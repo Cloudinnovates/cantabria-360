b321a89a27d16e0ceb667cab2ffbf55bab279c2f
Rubén Chavarría <txingo@gmail.com>
2019-03-26 22:02:14 +0100

    Make geographical coordinates not to depend on THREE library

--- src/infrastructure/three/geographic-coordinates.js
+++ src/infrastructure/three/geographic-coordinates.js
@@ -1,5 +1,12 @@
-import { Math as ThreeMath } from 'three'
 import Position from '../../domain/model/position'
+
+function clamp (value, min, max) {
+  return Math.max(min, Math.min(max, value))
+}
+
+function degreeToRadian (degree) {
+  return degree * Math.PI / 180
+}
 
 export default class GeographicCoordinates {
   constructor ({ longitude = 0, latitude = 0 }) {
@@ -7,21 +14,15 @@ export default class GeographicCoordinates {
     this.latitude = latitude
   }
 
-  moveLongitude (delta) {
-    const longitude = this.longitude + delta
-    const latitude = Math.max(-85, Math.min(85, this.latitude))
-    return new GeographicCoordinates({ longitude, latitude })
-  }
-
-  moveDelta (deltaX, deltaY) {
+  move (deltaX, deltaY) {
     const longitude = deltaX + this.longitude
-    const latitude = deltaY + this.latitude
+    const latitude = clamp(this.latitude + deltaY, -85, 85)
     return new GeographicCoordinates({ longitude, latitude })
   }
 
   position () {
-    const phi = ThreeMath.degToRad(90 - this.latitude)
-    const theta = ThreeMath.degToRad(this.longitude)
+    const phi = degreeToRadian(90 - this.latitude)
+    const theta = degreeToRadian(this.longitude)
     return new Position(
       500 * Math.sin(phi) * Math.cos(theta),
       500 * Math.cos(phi),
