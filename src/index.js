1f5177bd5c54ee04d8bd3e6fa526a771d9661c12
Rubén Chavarría <txingo@gmail.com>
2019-03-27 23:17:22 +0100

    Fetch tour from a repository

--- src/index.js
+++ src/index.js
@@ -21,21 +21,13 @@ function configureEventListeners () {
   window.addEventListener('resize', context.resize.bind(context), false)
 }
 
-function animate () {
-  requestAnimationFrame(animate)
-  context.update()
-}
-
 const tourId = browser.tourId()
-if (!tourId) {
-  const info = document.getElementById('info')
-  info.innerHTML = '<h1>No he podido encontrar un tour</h1>'
-} else {
-  fetch(`src/data/tour-${tourId}.json`)
-    .then(response => response.json())
+if (tourId) {
+  TourRepository.find(tourId)
     .then(jsonTour => {
       context.init(TourBuilder.fromJSON(jsonTour))
       configureEventListeners()
-      animate()
     })
+} else {
+  ShowError.tourIdNotFound()
 }
