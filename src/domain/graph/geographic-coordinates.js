b6f98f41c941f888cf794d7cf3528a5e837c4aab
Rubén Chavarría <txingo@gmail.com>
2019-06-19 22:35:40 +0200

    :white_check_mark: Add tests for geographic coordinates

--- src/domain/graph/geographic-coordinates.js
+++ src/domain/graph/geographic-coordinates.js
@@ -1,11 +1,15 @@
 import Position from '../model/position'
 
+const DEGREES_BY_PI_RADIANS = 180
+const MAX_VISIBLE_DEGREES = 85
+const RADIUS = 500
+
 function clamp (value, min, max) {
   return Math.max(min, Math.min(max, value))
 }
 
 function degreeToRadian (degree) {
-  return degree * Math.PI / 180
+  return degree * Math.PI / DEGREES_BY_PI_RADIANS
 }
 
 export default class GeographicCoordinates {
@@ -16,17 +20,17 @@ export default class GeographicCoordinates {
 
   move (deltaX, deltaY) {
     const longitude = deltaX + this.longitude
-    const latitude = clamp(this.latitude + deltaY, -85, 85)
+    const latitude = clamp(this.latitude + deltaY, -MAX_VISIBLE_DEGREES, MAX_VISIBLE_DEGREES)
     return new GeographicCoordinates({ longitude, latitude })
   }
 
   position () {
-    const phi = degreeToRadian(90 - this.latitude)
+    const phi = degreeToRadian((DEGREES_BY_PI_RADIANS / 2) - this.latitude)
     const theta = degreeToRadian(this.longitude)
     return new Position(
-      500 * Math.sin(phi) * Math.cos(theta),
-      500 * Math.cos(phi),
-      500 * Math.sin(phi) * Math.sin(theta)
+      RADIUS * Math.sin(phi) * Math.cos(theta),
+      RADIUS * Math.cos(phi),
+      RADIUS * Math.sin(phi) * Math.sin(theta)
     )
   }
 }
