04f8dfc817c89641151c63b4c8565c1400eb600a
Rubén Chavarría <txingo@gmail.com>
2019-06-30 19:00:05 +0200

    :white_check_mark: Add test for building one room with gates

--- test/unit/domain/builders/tour-builder.spec.js
+++ test/unit/domain/builders/tour-builder.spec.js
@@ -10,6 +10,13 @@ function jsonRoom () {
   }
 }
 
+function jsonGate () {
+  return {
+    id: 'irrelevant-gate-id',
+    position: { x: 1, y: 2, z: 3 }
+  }
+}
+
 describe('GeographicCoordinates', () => {
   describe('#fromJSON', () => {
     let definition
@@ -39,15 +46,31 @@ describe('GeographicCoordinates', () => {
     })
 
     it('builds one room without gates', () => {
-      definition.rooms = [ jsonRoom() ]
+      definition.rooms.push(jsonRoom())
       const tour = TourBuilder.fromJSON(definition)
       expect(tour.rooms.length).toEqual(1)
     })
 
     it('builds several rooms without gates', () => {
-      definition.rooms = [ jsonRoom(), jsonRoom(), jsonRoom() ]
+      definition.rooms.push(jsonRoom())
+      definition.rooms.push(jsonRoom())
+      definition.rooms.push(jsonRoom())
       const tour = TourBuilder.fromJSON(definition)
       expect(tour.rooms.length).toEqual(3)
+    })
+
+    it('builds one room with one gate', () => {
+      const gate = jsonGate()
+      definition.gates.push(gate)
+
+      const room = jsonRoom()
+      room.gates.push(gate.id) // room's gates are linked by id
+      definition.rooms.push(room)
+
+      const tour = TourBuilder.fromJSON(definition)
+
+      expect(tour.rooms[0].gates.length).toEqual(1)
+      expect(tour.rooms[0].gates[0].id).toEqual(gate.id)
     })
   })
 })
